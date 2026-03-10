b5f31ca3f3c30a1bdec62b1d21c8ce324d8fa8ec
Starseth <47060055+Starseth@users.noreply.github.com>
2026-03-10 11:54:25 -0400

    Initial App commit

---
+++ app/src/main/java/com/zybooks/parkingapp/MainActivity.java
@@ -0,0 +1,38 @@
+package com.zybooks.parkingapp;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    String[] items = {"Clarkson","Potsdam"};
+
+    AutoCompleteTextView autoCompleteTextView;
+    ArrayAdapter<String> adapterItems;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        autoCompleteTextView = findViewById(R.id.auto_complete_text);
+        adapterItems = new ArrayAdapter<String>(this, R.layout.list_items, items);
+        autoCompleteTextView.setAdapter(adapterItems);
+
+        autoCompleteTextView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String item = parent.getItemAtPosition(position).toString();
+                Toast.makeText(getApplicationContext(), item, Toast.LENGTH_SHORT).show();
+            }
+        });
+        }
+
+    }
