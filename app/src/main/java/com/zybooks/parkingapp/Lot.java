77b846f887c1390a66fba42415be7a90b3193fb1
Starseth <47060055+Starseth@users.noreply.github.com>
2026-04-16 13:16:40 -0400

    added Yohans stuff p1.

---
+++ app/src/main/java/com/zybooks/parkingapp/Lot.java
@@ -0,0 +1,106 @@
+package com.zybooks.parkingapp;
+
+public class Lot {
+    private String lotName;
+    private int lotnumber;
+    private int totalLotnumber;
+
+    /**
+     * Constructor for the Lot class
+     */
+    public Lot(String lotName, int lotnumber, int totalLotnumber) {
+        this.lotName = lotName;
+        this.lotnumber = lotnumber;
+        this.totalLotnumber = totalLotnumber;
+    }
+
+
+
+
+
+
+
+
+    /**
+     * Get the name of the lot
+     *
+     * @return lot name
+     */
+    public String getLotName() {
+        return lotName;
+    }
+
+    /**
+     * Get the ID of the lot (using name as ID)
+     *
+     * @return lot name
+     */
+    public String getLotId() {
+        return lotName;
+    }
+
+    /**
+     * Set the name of the lot
+     */
+    public void setLotName(String lotName) {
+        this.lotName = lotName;
+    }
+
+    /**
+     * getLot_number
+     * @return lot_number
+     */
+    public int getLotnumber() {
+        return lotnumber;
+    }
+
+    /**
+     * setLot_number
+     * @param lotnumber
+     */
+    public void setLotnumber(int lotnumber) {
+        this.lotnumber = lotnumber;
+    }
+
+    /**
+     * get total_lot_number
+     *
+     * @return total_lot_number
+     */
+    public int getTotalLotnumber() {
+        return totalLotnumber;
+    }
+
+    /**
+     * set total_lot_number
+     *
+     * @param totalLotnumber
+     */
+    public void setTotalLotnumber(int totalLotnumber) {
+        this.totalLotnumber = totalLotnumber;
+    }
+
+    /**
+     * Get Status of the lot is the method that
+     * provide how many spot are available in the lot
+     * out of the full capacity
+     *
+     * @return lot number /total number lot
+     */
+    public String getStatus() {
+        return  lotnumber + " / " + totalLotnumber;
+    }
+
+    /*Optional isFull() and isEmpty() this would be boolean function that check if the lot is full or empty
+     */
+
+    // using logic >= for security and testing
+    public boolean isFull() {
+        return lotnumber >= totalLotnumber;
+    }
+
+    public boolean isEmpty() {
+
+        return lotnumber == 0;
+    }
+}
